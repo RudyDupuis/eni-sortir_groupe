cead8daf50b6ccc7dea2f6d2af091f4b8b813f63
Vincent BLANCHET <vbm.maconnerie@gmail.com>
2023-11-15 00:24:45 +0100

    Gestion de profil - Vincent Modification mineur Participant ( Ajout de l'unicité pseudo dans Symfony pour futur formulaire en plus de celle de la colonne en BDD)

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('mail', EmailType::class, [
+                'label' => 'Mail'
+            ])
+            ->add('motPasse', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo'
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prenom'
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+                'label' => 'Choix du campus',
+                'expanded' => true,
+                'multiple' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
