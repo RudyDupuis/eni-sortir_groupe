8b6788db97b8d1f84d85fb051e2676f316944c2f
Henrik <henrik.hersant@gmail.com>
2023-11-15 19:53:34 +0100

    page sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,14 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class SortieController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request, Security $security): Response
     {
-        return $this->render('pages/accueil.html.twig');
+        // Récupérer la date actuelle
+        $currentDate = new \DateTime();
+
+        // Récupérer l'utilisateur connecté
+        $user = $security->getUser();
+
+        // Rendre la vue en passant la date et l'utilisateur
+        return $this->render('pages/accueil.html.twig', [
+            'currentDate' => $currentDate,
+            'user' => $user,
+        ]);
     }
 }
